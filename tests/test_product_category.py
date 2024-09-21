bf17e250c7319200d8346158086db840dc192c78
Svetlana Bazhenova <varta.73@mail.ru>
2024-09-21 15:51:18 +0300

    Добавление файла README.md

--- tests/test_product_category.py
+++ tests/test_product_category.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-def test_category_init(category1, category2):
+def test_category(category1, category2):
     assert category1.name == "Смартфоны"
     assert (
         category1.description
