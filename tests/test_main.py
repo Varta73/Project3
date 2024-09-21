60ecd7b27083f08f5d08d48b10c33451a8769746
Svetlana Bazhenova <varta.73@mail.ru>
2024-09-21 15:32:29 +0300

    Добавление тестов

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,24 +1,28 @@
 import pytest
+
 from src.product_category import Category, Product
 
 
-def test_category_init(category1, category2):
+def test_category_init(category1: Category, category2: Category) -> None:
     assert category1.name == "Смартфоны"
-    assert category1.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    assert (
+        category1.description
+        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    )
     assert len(category1.products) == 3
 
     assert category1.category_count == 2
     assert category2.product_count == 4
 
 
-def test_product(product):
-    assert product.name == "55\" QLED 4K"
+def test_product(product: Product) -> None:
+    assert product.name == '55" QLED 4K'
     assert product.description == "Фоновая подсветка"
     assert product.price == 123000.0
     assert product.quantity == 7
 
 
-def test_count_category_products():
+def test_count_category_products() -> None:
     product1 = Product("Iphone 16", "Apple", 180000.0, 5)
     product2 = Product("Galaxy a55", "Samsung", 250000.0, 8)
     category = Category("Смартфоны", "Телефоны для связи", [product1, product2])
@@ -28,9 +32,9 @@ def test_count_category_products():
     assert product1.quantity + product2.quantity == 13
 
 
-def test_add_product():
-    product1 = Product("Товар 3", "Описание товара 3", 200.0, 2)
-    product2 = Product("Товар 3", "Описание товара 3", 200.0, 2)
+def test_add_product() -> None:
+    product1 = Product("Товар 1", "Описание товара 1", 100.0, 2)
+    product2 = Product("Товар 2", "Описание товара 2", 200.0, 2)
     sum_product = product1.price * product1.quantity + product2.price * product2.quantity
 
-    assert sum_product == 800.0
+    assert sum_product == 600.0
--- tests/test_product_category.py
+++ tests/test_product_category.py
@@ -1,8 +1,12 @@
 import pytest
 
+
 def test_category_init(category1, category2):
     assert category1.name == "Смартфоны"
-    assert category1.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    assert (
+        category1.description
+        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    )
     assert len(category1.products) == 3
 
     assert category1.category_count == 2
@@ -10,7 +14,7 @@ def test_category_init(category1, category2):
 
 
 def test_product(product):
-    assert product.name == "55\" QLED 4K"
+    assert product.name == '55" QLED 4K'
     assert product.description == "Фоновая подсветка"
     assert product.price == 123000.0
     assert product.quantity == 7
