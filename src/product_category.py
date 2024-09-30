5f592f8f25eb17f35e7d266f9f9647f4aef116a8
Svetlana Bazhenova <varta.73@mail.ru>
2024-09-30 23:37:48 +0300

    Добавление для классов Product и Category строкового отображения, оптимизация геттера

--- src/product_category.py
+++ src/product_category.py
@@ -13,6 +13,14 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f"{self.name}, {self.__price}. Остаток: {self.quantity}"
+
+
+    def __add__(self, other):
+        return self.__price * self.quantity + other.__price * other.quantity
+
+
     @classmethod
     def new_product(cls, product_dict: dict) -> Any:
         new_prod = cls(
@@ -49,6 +57,9 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(products)
 
+    def __str__(self):
+        return f"{self.name}, количество продуктов: {len(self.__products)}"
+
     def add_product(self, product: Product) -> None:
         self.__products.append(product)
         Category.product_count += 1
@@ -57,5 +68,5 @@ class Category:
     def products(self) -> str:
         prod: str = ""
         for product in self.__products:
-            prod += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+            prod += f"{str(product)}\n"
         return prod
